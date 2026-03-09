40335f0681a3a64de2e8942367d81255221bdd1f
FICup <fabry.amoroso@gmail.com>
2026-03-09 13:26:54 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,13 @@
+{
+  "background_color": "#ffffff",
+  "dir": "ltr",
+  "display": "standalone",
+  "name": "Admin",
+  "orientation": "any",
+  "scope": "/",
+  "short_name": "Admin",
+  "start_url": "https://ficup26.github.io/admin/",
+  "theme_color": "#ffffff",
+  "id": "Admin",
+  "description": "Nessuna"
+}
